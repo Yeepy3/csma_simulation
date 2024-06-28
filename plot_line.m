4123bd0bae5b179fe784a9f66d34a736385ef8c5
ybchoi <ybchoi24@netlab.snu.ac.kr>
2024-06-28 14:37:10 +0900

    yb commit for indent

--- plot_line.m
+++ plot_line.m
@@ -1,47 +1,47 @@
 function plot_line
 
-global plot_flag;
-n=size(plot_flag,1);
-xline=zeros(n,100);
-yline=zeros(n,100);
+    global plot_flag;
+    n=size(plot_flag,1);
+    xline=zeros(n,100);
+    yline=zeros(n,100);
 
 
 
-for i=1:n
-    if plot_flag(i,1)==1
-        if plot_flag(i,2)==1
-            type='y';
-        else
-            type='g';
-        end
-        h(i) = animatedline('Color',type,'LineWidth',3);
-        xline(i,:) = linspace(plot_flag(i,3),plot_flag(i,5),100);
-        yline(i,:) = linspace(plot_flag(i,4),plot_flag(i,6),100);        
-    end
-
-end
-
-
-for k = 1:100
     for i=1:n
         if plot_flag(i,1)==1
-            addpoints(h(i),xline(i,k),yline(i,k));
-            drawnow
+            if plot_flag(i,2)==1
+                type='y';
+            else
+                type='g';
+            end
+            h(i) = animatedline('Color',type,'LineWidth',3);
+            xline(i,:) = linspace(plot_flag(i,3),plot_flag(i,5),100);
+            yline(i,:) = linspace(plot_flag(i,4),plot_flag(i,6),100);        
+        end
+
+    end
+
+
+    for k = 1:100
+        for i=1:n
+            if plot_flag(i,1)==1
+                addpoints(h(i),xline(i,k),yline(i,k));
+                drawnow
+            end
         end
     end
-end
 
-for i=1:n
-    if plot_flag(i,1)==1
-        clearpoints(h(i))
+    for i=1:n
+        if plot_flag(i,1)==1
+            clearpoints(h(i))
+        end
     end
-end
 
 
 
 
 
-plot_flag=zeros(n+1,6);
+    plot_flag=zeros(n+1,6);
 
 
 end
